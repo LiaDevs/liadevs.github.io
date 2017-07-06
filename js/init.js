c5e01ba11f03a907a481592d7cc17a0af423b747
defund <will.austin.wang@gmail.com>
2017-07-05 21:55:52 -0400

    initial website

---
+++ js/init.js
@@ -0,0 +1,16 @@
+$(function() {
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+  });
+})(jQuery);
+
